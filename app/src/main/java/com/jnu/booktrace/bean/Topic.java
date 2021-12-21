4130793c29bc96d987b4ecac7e718d0d4f11674b
StreamAzure <1905105036@qq.com>
2021-12-21 18:32:45 +0800

    update:自由谈话题页、主题贴页的基本框架及相关数据类

---
+++ app/src/main/java/com/jnu/booktrace/bean/Topic.java
@@ -0,0 +1,26 @@
+package com.jnu.booktrace.bean;
+
+public class Topic {
+    private String title;
+    private String content;
+    public Topic(String title,String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- app/src/main/java/com/jnu/booktrace/database/DatabaseHelper.java
+++ app/src/main/java/com/jnu/booktrace/database/DatabaseHelper.java
@@ -17,17 +17,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         //创建用户信息表
-        String sql = "create table persontb (id integer primary key autoincrement, name varchar(25) not null unique, " +
+        String sqlCreatePersonTable = "create table persontb (id integer primary key autoincrement, name varchar(25) not null unique, " +
                 "password varchar(50) not null, nickname varchar(20), description varchar(100))";
-        db.execSQL(sql);
+        //自由谈：话题（后台发布），ID，话题名称，话题描述，参与用户（字符串格式维护一个用户ID列表），相关主题数量
+        String sqlCreateTopicTable = "create table topictb(id integer primary key autoincrement, title varchar(200) not null, "+
+                "description text, subscribers text, postCount integer)";
+        //自由谈：主题帖（用户在某一话题下发布）,ID，内容，日期（yyyy-MM-dd格式字符串），作者用户ID（一个整数），所属话题ID，赞同数，回复数
+        String sqlCreatePostTable = "create table posttb(id integer primary key autoincrement, "+
+                "content text not null, date text, author integer, relativeTopic integer, likeCount integer, replyCount integer)";
+        //自由谈：回复（在主题贴下由用户发布），ID，内容，日期（yyyy-MM-dd格式字符串），作者用户ID，所属主题帖ID，赞同数
+        String sqlCreateReplyTable = "create table replytb(id integer primary key autoincrement, content text not null, date text, "+
+                "author integer, relativePost integer, likeCount integer)";
 
-        //创建书籍基本信息表，ISBN号均可作为主键
-        String createBookTable = "create table booktb (isbn varchar(15) not null unique, " +
-                "id varchar(25), title text, image text, author varchar(100), translator varchar(100), " +
-                "publisher varchar(100), pubdate varchar(25), tags varchar(100), binding varchar(20), price varchar(25), "+
-                "pages integer, author_intro text, summary text)";
-        db.execSQL(createBookTable);
-
+        db.execSQL(sqlCreatePersonTable);
+        db.execSQL(sqlCreateTopicTable);
+        db.execSQL(sqlCreatePostTable);
+        db.execSQL(sqlCreateReplyTable);
     }
 
     //数据库版本发生改变时调用
