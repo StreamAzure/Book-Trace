98327a39e389f380f7ab86b1c1e3acdb5bad79d1
hu-hamster <hj.325915>
2021-12-27 13:38:50 +0800

    添加数据

---
+++ app/src/main/java/com/jnu/booktrace/drift/DriftInfoActivity.java
@@ -0,0 +1,15 @@
+package com.jnu.booktrace.drift;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jnu.booktrace.R;
+
+public class DriftInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drift_info);
+    }
+}
